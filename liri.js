26260b6a9d47dbbab9a99263622288bab60c9fc8
nevermindthelabel <nevermindthelabel@gmail.com>
2018-12-16 16:32:38 -0700

    added code to pull in the user input for the liri app search function

--- liri.js
+++ liri.js
@@ -5,19 +5,23 @@ var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs")
 
-console.log(process.env.SPOTIFY_ID);
-console.log(process.env.SPOTIFY_SECRET);
-console.log(process.env.BANDS_IN_TOWN_API_KEY);
-console.log(process.env.OMDB_API_KEY);
+// console.log(process.env.SPOTIFY_ID);
+// console.log(process.env.SPOTIFY_SECRET);
+// console.log(process.env.BANDS_IN_TOWN_API_KEY);
+// console.log(process.env.OMDB_API_KEY);
 
 var bands = keys.bandsintown.api_key;
 var spotify = new Spotify(keys.spotify);
 var omdb = keys.omdb.api_key;
 
+
+// logic for taking the user input from the console
+
 var firstUserInput = process.argv[2];
 var secondUserInput = process.argv.slice(3).join(" ");
-console.log(secondUserInput)
-// var join = secondUserInput.join(' ')
+var logText = `\n${firstUserInput} ${secondUserInput}`
+
+// logic for taking the user input and making sure it is correct, or outputting an error message if it is not.
 
 if (firstUserInput === 'concert-this') {
     bandsInTown(secondUserInput);
@@ -27,21 +31,29 @@ if (firstUserInput === 'concert-this') {
     movieThis(secondUserInput);
 } else if (firstUserInput === 'do-what-it-says') {
     doThis(secondUserInput);
-}else {
-    return console.log('I\'m sorry, that is not a valid option')
+} else {
+    return console.log('I\'m sorry, that is not a valid option. Please enter concert this, spotify-this-song, movie-this, or do-what-it-says.')
 }
 
+fs.appendFile('./log.txt', logText, function(error){
+    if (error){
+        console.log(error)
+    } else{
+        console.log('Search added to log!')
+    }
+})
 
-
+// calls the Bands in Town API using Axios
 function bandsInTown() {
 
     axios
         .get(
-            "https://rest.bandsintown.com/artists/" + secondUserInput +"/events?app_id=" + bands)
+            "https://rest.bandsintown.com/artists/" + secondUserInput + "/events?app_id=" + bands)
         .then(function (response) {
             var artistName = response.data[0].lineup[0];
             var nameOfVenue = response.data[0].venue.name;
             var locationOfVenue = response.data[0].venue.city;
+            // using momentjs to search for the time
             var eventDate = moment(response.data[0].datetime).format('MM/DD/YYYY hh:mm a');
             console.log('Name of the artist: ' + artistName);
             console.log('                        ');
@@ -61,57 +73,64 @@ function bandsInTown() {
             else if (error.request); {
                 console.log(error.request);
             }
-           
+
         })
-    
+
+}
+// calls the spotify constructor.
+function spotifyThis(searchTerm) {
+    // sets a default song if the user doesn't enter one.
+    if (!searchTerm) {
+        searchTerm = 'The Sign', 'ace of base'
+    }
+    spotify
+        .search({
+            type: 'track',
+            query: searchTerm
+        }).then(function (response) {
+            var data = response.tracks.items[0];
+            var artist = data.artists[0].name;
+            var preview = data.external_urls.spotify;
+            var songName = data.name;
+            var albumName = data.album.name;
+
+            console.log(`The name of the Artist is: ${artist}`);
+            console.log(`The Song title is : ${songName}`);
+            console.log(`A preview of the song can be found here: ${preview}`);
+            console.log(`The song came from the album named: ${albumName}`)
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+
 }
 
-function spotifyThis () {
-   spotify
-    .search({
-        type: 'track',
-        query: secondUserInput
-    }).then(function(response){
-        // console.log(response.tracks.items[0]);
-        var data = response.tracks.items[0];
-        var artist = data.artists[0].name;
-        var preview = data.external_urls.spotify;
-        var songName =  data.name;
-        var albumName = data.album.name;
-        // console.log(albumName)
-        console.log(`The name of the Artist is: ${artist}`);
-        console.log(`The Song title is : ${songName}`);
-        console.log(`A preview of the song can be found here: ${preview}`);
-        console.log(`The song came from the album named: ${albumName}`)
-    })
-   .catch(function(error){
-       console.log(error)
-   })
-   
-}
-function movieThis(){
+// calls the omdb api to search for movies.
+function movieThis(searchTerm) {
+    // sets a default movie if the user doesn't search for one.
+    if (!searchTerm) {
+        return console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/")
+    }
     axios
-        .get("https://www.omdbapi.com/?t=" + secondUserInput + "&y=&plot=short&apikey=" + omdb)
+        .get("https://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=" + omdb)
         .then(function (response) {
-            // console.log(response.data)
-            console.log(response.data.Title);
             console.log('                      ')
-            console.log(response.data.Year);
+            console.log(`The title of your movie is ${response.data.Title}`);
+            console.log('                      ')
+            console.log('The movie came out in ' + response.data.Year);
             console.log('                      ');
-            console.log(response.data.Ratings[0].Value);
+            console.log('The IMDB rating is ' + response.data.Ratings[0].Value);
             console.log('                      ');
-            console.log(response.data.Ratings[1].Value);
+            console.log('The Rotten Tomatoes rating is ' + response.data.Ratings[1].Value);
             console.log('                      ');
-            console.log(response.data.Country);
+            console.log('The movie was produced in ' + response.data.Country);
             console.log('                      ');
-            console.log(response.data.Language);
+            console.log('The language of the movie is ' + response.data.Language);
             console.log('                      ');
-            console.log(response.data.Plot);
+            console.log('The movie\'s Plot is ' + response.data.Plot);
             console.log('                      ');
-            console.log(response.data.Actors);
-            if (secondUserInput === undefined && secondUserInput !== ''){
-                console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/")
-            }
+            console.log('The stars of the movie where ' + response.data.Actors);
+
         })
         .catch(function (error) {
             if (error.response) {
@@ -122,8 +141,28 @@ function movieThis(){
             else if (error.request); {
                 console.log(error.request);
             }
-           
+
         })
-    }
-    
-    
+}
+// pulls the text from random.txt
+function doThis() {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+        if (error) {
+            return console.log(error)
+        }
+        console.log(data)
+        var splitData = data.split(',')
+       
+        console.log('                            ')
+        if (splitData[0] === 'spotify-this-song'){
+            spotifyThis(splitData[1])
+        } else if (splitData[0] === 'concert-this'){
+            bandsInTown(splitData[1])
+        } else if (splitData[0] === 'movie-this'){
+            movieThis(splitData[1])
+        } else {
+            console.log('I\'m sorry, I do not know what you want')
+        }
+    })
+}
+
